5b42e78beb2ad3181763dbe03cb943e042650f52
Alex <223952347+AlexC65@users.noreply.github.com>
2026-05-26 12:05:27 -0300

    Strategy02

--- src/NotificationManagement.Application/Common/Interfaces/INotificationStrategy.cs
+++ src/NotificationManagement.Application/Common/Interfaces/INotificationStrategy.cs
@@ -1,9 +1,11 @@
 using System.Threading.Tasks;
+using NotificationManagement.Domain.Entities;
 
 namespace NotificationManagement.Domain.Interfaces;
 
 public interface INotificationStrategy
 {
-    
+    Task SendNotificationAsync(Notification notification);
 }
 
+    
