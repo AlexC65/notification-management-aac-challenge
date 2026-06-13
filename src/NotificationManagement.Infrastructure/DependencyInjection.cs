efedf76dc67d628642b1fe9f58271248a7ae7c8f
Alex <223952347+AlexC65@users.noreply.github.com>
2026-06-13 17:22:28 -0300

    DependencyInjection

--- src/NotificationManagement.Infrastructure/DependencyInjection.cs
+++ src/NotificationManagement.Infrastructure/DependencyInjection.cs
@@ -2,20 +2,31 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using NotificationManagement.Application.Interfaces;
+using NotificationManagement.Domain.Interfaces;
+using NotificationManagement.Infrastructure.Auth;
 using NotificationManagement.Infrastructure.Persistence;
+using NotificationManagement.Infrastructure.Repositories;
 
 namespace NotificationManagement.Infrastructure;
-
-public  static class DependencyInjection
+/// <summary>
+/// Provides extension methods for registering infrastructure services.
+/// </summary>
+public static class DependencyInjection
 {
-    public static IServiceCollection addInfrastructure( 
+    public static IServiceCollection addInfrastructure(
         this IServiceCollection services,
         IConfiguration configuration)
     {
         // EF Core + PostgreSQL
-        services.AddDbContext<AppDbContext>(options => 
+        services.AddDbContext<AppDbContext>(options =>
             options.UseNpgsql(configuration.GetConnectionString("DefaultConnection"))
         );
+        // Repositories
+        services.AddScoped<IUserRepository, UserRepository>();
+        //JWT
+        services.AddSingleton<IJwtTokenGenerator, JwtTokenGenerator>();
+
 
         return services;
     }
